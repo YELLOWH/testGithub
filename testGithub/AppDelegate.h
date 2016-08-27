1d11018aab9050f47a957f052f72efb62169b40b
HZW <hzw@HZW.local>
2016-08-27 10:55:28 +0800

    init

---
+++ testGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGithub
+//
+//  Created by HZW on 16/8/27.
+//  Copyright © 2016年 HC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGithub/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  testGithub
+//
+//  Created by HZW on 16/8/27.
+//  Copyright © 2016年 HC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+    self.view.backgroundColor=[UIColor redColor];
+}
+
+@end
